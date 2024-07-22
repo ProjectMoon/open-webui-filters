1735f231732921332a5d6aa4350201e86c49d05a
projectmoon <projectmoon@agnos.is>
2024-07-22 20:23:11 +0200

    more agpl

--- memories.py
+++ memories.py
@@ -3,6 +3,7 @@ title: Memory Filter
 author: projectmoon
 author_url: https://git.agnos.is/projectmoon/open-webui-filters
 version: 0.0.2
+license: AGPL-3.0+
 required_open_webui_version: 0.3.9
 """
 
