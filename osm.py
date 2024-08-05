a2a1a2624decbc2586d57458c7be6165a73f6041
projectmoon <projectmoon@agnos.is>
2024-08-05 10:13:55 +0200

    OSM 0.2.3

--- osm.py
+++ osm.py
@@ -2,7 +2,7 @@
 title: OpenStreetMap Tool
 author: projectmoon
 author_url: https://git.agnos.is/projectmoon/open-webui-filters
-version: 0.2.2
+version: 0.2.3
 license: AGPL-3.0+
 required_open_webui_version: 0.3.9
 """
